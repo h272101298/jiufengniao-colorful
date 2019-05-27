f03158c92a21e86e30c66b61d254ff041f3b9bdd
DevZeng <zengshungeng@gmail.com>
2019-05-27 16:14:24 +0800

    fux

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -18,6 +18,7 @@ class OrderController extends Controller
 
     public function makeOrder(Request $post)
     {
+
         $userHandle = new UserHandle();
         $type = $post->type?$post->type:'origin';
         $product_id = $post->product_id;
@@ -61,14 +62,103 @@ class OrderController extends Controller
                 'city'=>$address->city,
                 'address'=>$address->address,
                 'phone'=>$address->phone,
+                'order_id'=>$result
             ];
-            $this->handle->addOrderAddress($result,$addressData);
+            $this->handle->addOrderAddress(0,$addressData);
         }
         return jsonResponse([
             'msg'=>'ok',
             'data'=>[
                 'number'=>$orderSn
             ]
+        ]);
+    }
+    public function payOrder(Request $post)
+    {
+        $url = $post->getScheme() . '://' . $post->getHttpHost() . '/api/pay/notify';
+        $user_id = getRedisData($post->token);
+        $order_id = $post->order_id;
+        $repay = $post->repay?$post->repay:0;
+        $user = WeChatUser::findOrFail($user_id);
+        $amountPay = $post->amountPay?$post->amountPay:0;
+        $goodsCheck=$this->handle->goodsNumCheck($order_id);
+        if(!$goodsCheck){
+            return response()->json([
+                'msg'=>'库存不足!'
+            ],400);
+        }
+        if ($repay){
+            $order = Order::where('number', '=', $order_id)->first();
+            if ($order->state!='created'){
+                return jsonResponse([
+                    'msg' => '订单已支付！'
+                ], 400);
+            }
+            if ($amountPay){
+                $amount = $this->handle->getUserAmount($user_id);
+                if ($amount<$order->price){
+                    throw new \Exception('余额不足！');
+                }
+                $this->handle->setUserAmount($user_id,$amount-$order->price);
+                $data = [
+                    'state' => 'paid'
+                ];
+                $this->handle->addOrder($order->id,$data);
+                return response()->json([
+                    'msg' => 'ok'
+                ]);
+            }
+            //$price = Order::where('group_number', '=', $order_id)->sum('price');
+            $wxPay = getWxPay($user->open_id);
+            $data = $wxPay->pay($order_id, '购买商品', ($order->price) * 100, $url);
+            $notify_id = $wxPay->getPrepayId();
+            Order::where('number', '=', $order_id)->update(['notify_id' => $notify_id]);
+            return response()->json([
+                'msg' => 'ok',
+                'data' => $data
+            ]);
+        }
+        $count = Order::where('group_number', '=', $order_id)->where('state', '!=', 'created')->count();
+        if ($count != 0) {
+            return jsonResponse([
+                'msg' => '订单已支付！'
+            ], 400);
+        }
+        $order_user = Order::where('group_number', '=', $order_id)->pluck('user_id')->first();
+        if ($order_user != $user_id) {
+            return jsonResponse([
+                'msg' => '无权操作！'
+            ], 403);
+        }
+        $price = Order::where('group_number', '=', $order_id)->sum('price');
+        $product_id=$this->handle->orderId_exchange_productId($order_id);
+        $res=$this->handle->goodsNumDec($product_id);
+        if(!$res){
+            return response()->json([
+                'msg'=>'库存不足!'
+            ],400);
+        }
+        if ($amountPay){
+            $amount = $this->handle->getUserAmount($user_id);
+            if ($amount<$price){
+                throw new \Exception('余额不足！');
+            }
+            $this->handle->setUserAmount($user_id,$amount-$price);
+            $data = [
+                'state' => 'paid'
+            ];
+            Order::where('group_number', '=', $order_id)->update(['state'=>'paid']);
+            return response()->json([
+                'msg' => 'ok'
+            ]);
+        }
+        $wxPay = getWxPay($user->open_id);
+        $data = $wxPay->pay($order_id, '购买商品', ($price) * 100, $url);
+        $notify_id = $wxPay->getPrepayId();
+        Order::where('group_number', '=', $order_id)->update(['notify_id' => $notify_id]);
+        return response()->json([
+            'msg' => 'ok',
+            'data' => $data
         ]);
     }
 }
