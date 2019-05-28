b3eba7fcb5161fb2f5e52dac53e48ebd2c8b5194
DevZeng <zengshungeng@gmail.com>
2019-05-28 15:11:46 +0800

    fix

--- app/Modules/Sign/SignHandle.php
+++ app/Modules/Sign/SignHandle.php
@@ -27,5 +27,34 @@ class SignHandle
         $configs = SignConfig::all();
         return $configs;
     }
-
+    public function addSignRecord($user_id)
+    {
+        $record = new SignRecord();
+        $record->user_id = $user_id;
+        if ($record->save()){
+            return true;
+        }
+        return false;
+    }
+    public function checkSign($user_id,$today=1)
+    {
+        $db = SignRecord::where('user_id','=',$user_id);
+        if ($today){
+            $db->whereDate('created_at', date('Y-m-d',time()));
+        }
+        return $db->count();
+    }
+    public function setContinueSign($user_id,$data)
+    {
+        $data = serialize($data);
+        setRedisData('sign'.$user_id,$data);
+    }
+    public function getContinueSign($user_id)
+    {
+        $data = getRedisData('sign'.$user_id);
+        if (!empty($data)){
+            return unserialize($data);
+        }
+        return false;
+    }
 }
