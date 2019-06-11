e4377b94c66048660496c5693f6db6801dfa730d
DevZeng <zengshungeng@gmail.com>
2019-06-11 17:42:59 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -59,5 +59,5 @@ Route::group(['middleware'=>['auth']],function (){
     Route::post('notify/config','SystemController@setNotifyConfig');
     Route::get('notify/configs','SystemController@getNotifyConfigs');
     Route::get('notify/config','SystemController@getNotifyConfig');
-    Route::delete('notify/config','SystemController@delNotifyConfig');
+    Route::get('del/notify/config','SystemController@delNotifyConfig');
 });
