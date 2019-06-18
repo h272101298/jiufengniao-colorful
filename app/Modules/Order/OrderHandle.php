527b260f0ff23c913e335d0641d310e6ea7e88b6
DevZeng <zengshungeng@gmail.com>
2019-06-18 15:47:56 +0800

    fix

--- app/Modules/Order/OrderHandle.php
+++ app/Modules/Order/OrderHandle.php
@@ -159,7 +159,7 @@ class OrderHandle
     public function getExpressInfo($order_id)
     {
         $order = Order::find($order_id);
-        $search = new ExpressSearch('1542740','a8fa1de3-7e0d-4f14-a193-64267ebdaaad');
+        $search = new ExpressSearch('1358357','4ca2f3ce-5025-4d03-af5a-60bb64e1ffd1');
         $express = Express::find($order->express_id);
         $data = $search->getOrderTracesByJson($express->code,$order->express_number);
         $data = json_decode($data);
